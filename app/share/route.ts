2933fbbbf0b7842316ccb0cfc3fee7f518952004
Vladislav Gulevitc <v.gulevitc@icloud.com>
2025-09-22 10:58:42 +0300

    yet another logs clean up

--- app/share/route.ts
+++ app/share/route.ts
@@ -3,28 +3,22 @@ import { NextRequest, NextResponse } from "next/server";
 import { Quote, QuoteR } from "../api/v1/fortune/route";
 
 const kinds: Record<string, string> = {
-  "X": "https://twitter.com/intent/tweet?text=",
-  "Bluesky": "https://bsky.app/intent/compose?text=",
+  X: "https://twitter.com/intent/tweet?text=",
+  Bluesky: "https://bsky.app/intent/compose?text=",
 };
 
 export async function GET(req: NextRequest) {
-  console.log("SHARE START");
-  
   const url = new URL(req.url);
   const kindParam = url.searchParams.get("kind") || "X";
   const idParam = url.searchParams.get("id") || "21023";
-  const base = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
-  console.log(kinds[kindParam]);
-  console.log(123);
-  
-  
-  if(kinds[kindParam] === "" || kinds[kindParam] === undefined || Number.isNaN(Number(idParam))
-  ){
-    console.log(kindParam);
-    console.log("REDIR");
-    
-    
-    return NextResponse.redirect(new URL(("/?id=21023"), url))
+  const base = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+  if (
+    kinds[kindParam] === "" ||
+    kinds[kindParam] === undefined ||
+    Number.isNaN(Number(idParam))
+  ) {
+    return NextResponse.redirect(new URL("/?id=21023", url));
   }
 
   const apiUrl = new URL(`/api/v1/fortune?id=${idParam}`, base);
@@ -33,10 +27,10 @@ export async function GET(req: NextRequest) {
   const data = await resp.json().catch(() => null);
   const quote = data as QuoteR;
 
-  const shareText = `${quote.text}\n\n ${base}/?id=${quote.id}`
+  const shareText = `${quote.text}\n\n ${base}/?id=${quote.id}`;
 
-  console.log(kinds[kindParam]);
-  
-  const res = NextResponse.redirect(new URL((kinds[kindParam]) + encodeURIComponent(shareText)));
+  const res = NextResponse.redirect(
+    new URL(kinds[kindParam] + encodeURIComponent(shareText))
+  );
   return res;
 }
